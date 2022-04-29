de0d9c3439dce67c45045d18a538b3be364b5ce2
정상헌 <ertr@jeongsangheon-ui-MacBookPro.local>
2022-04-29 17:17:33 +0900

    nicknames part one

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -9,4 +11,23 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+// http, ws 둘다 돌릴 수 있는 서버 생성
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+
+  console.log("Connected to Browser!");
+  socket.on("close", () => {
+    console.log("Disconnected from the browser");
+  });
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
+});
+
+server.listen(3000, handleListen);
